d003d1c684f3eabdd6bf66248eb4f4ab2f3ce8b4
geoffrey zhang <west618517@gmail.com>
2020-07-16 14:25:12 +0800

    42

--- upload_ttest.py
+++ upload_ttest.py
@@ -123,4 +123,6 @@ def upload():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    from waitress import serve
+    # serve(app,host='0.0.0.0',port=5005)
+    app.run(host='0.0.0.0',port=5005,debug=True)
